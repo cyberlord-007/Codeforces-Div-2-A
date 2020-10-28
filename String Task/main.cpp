7662ea06d9851e50e691cdc85fee1361ce75cc27
cyberlord-007 <akashfzd2000@gmail.com>
2020-10-28 14:50:47 +0530

    Prob 3

---
+++ String Task/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+typedef long long int ll;
+#define w(t)  int t; cin>>t; while(t--)
+#define dec(x,y)  fixed<<setprecision(y)<<x
+#define M  1000000007
+
+
+int main() 
+{ 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+  
+#ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("error.txt", "w", stderr); 
+    freopen("output.txt", "w", stdout); 
+#endif 
+  
+    string s;
+    cin>>s;
+    string res;
+    for(int i=0;i<s.length();i++){
+        if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+            continue;
+        else{
+            res += '.';
+            res += tolower(s[i]);
+        }
+    }
+    cout<<res<<endl;  
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
+    return 0; 
+} 
